f15b8b8a6568077f79ba281db2d28730f867c3f6
polinayermolenko <polina.shvajukova@gmail.com>
2020-11-24 13:45:48 +0300

    Delete TaskDate

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,42 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { formatDistanceToNow } from 'date-fns';
 import PropTypes from 'prop-types';
 import TaskEdit from '../TaskEdit/TaskEdit';
-import TaskDate from '../TaskDate/TaskDate';
 
-const Task = ({
-  id,
-  onDeleted,
-  onBlur,
-  onEdit,
-  onToggleCompleted,
-  onToggleEditing,
-  onEscPress,
-  label,
-  editing,
-  completed,
-  time,
-}) => {
-  return (
-    <li key={id} className={`${completed ? 'completed' : ''} ${editing ? 'editing' : ''}`}>
-      <div className="view">
-        <input className="toggle" type="checkbox" onClick={onToggleCompleted} defaultChecked={completed} />
-        <label>
-          <span className="description">{label}</span>
-          <TaskDate time={time} />
-        </label>
-        <button
-          type="button"
-          aria-label="Edit Task"
-          className={`${completed ? 'icon icon-edit icon-edit-disabled' : 'icon icon-edit'}`}
-          onClick={onToggleEditing}
-          disabled={completed}
-        />
-        <button type="button" aria-label="Delete Task" className="icon icon-destroy" onClick={onDeleted} />
-      </div>
-      {editing ? <TaskEdit onEscPress={onEscPress} onBlur={onBlur} onEdit={onEdit} label={label} id={id} /> : null}
-    </li>
-  );
-};
+export default class Task extends Component {
+  intervalId = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: props.time,
+      interval: formatDistanceToNow(props.time, { includeSeconds: true }),
+    };
+  }
+
+  componentDidMount() {
+    const { updateInterval } = this.props;
+    this.intervalId = setInterval(() => this.getTime(), updateInterval);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  getTime() {
+    const { time } = this.state;
+    this.setState(() => ({ interval: formatDistanceToNow(time, { includeSeconds: true }) }));
+  }
+
+  render() {
+    const {
+      id,
+      onDeleted,
+      onBlur,
+      onEdit,
+      onToggleCompleted,
+      onToggleEditing,
+      onEscPress,
+      label,
+      editing,
+      completed,
+    } = this.props;
+
+    const { interval } = this.state;
+
+    return (
+      <li key={id} className={`${completed ? 'completed' : ''} ${editing ? 'editing' : ''}`}>
+        <div className="view">
+          <input className="toggle" type="checkbox" onClick={onToggleCompleted} defaultChecked={completed} />
+          <label>
+            <span className="description">{label}</span>
+            <span className="created">created {interval} ago</span>
+          </label>
+          <button
+            type="button"
+            aria-label="Edit Task"
+            className={`${completed ? 'icon icon-edit icon-edit-disabled' : 'icon icon-edit'}`}
+            onClick={onToggleEditing}
+            disabled={completed}
+          />
+          <button type="button" aria-label="Delete Task" className="icon icon-destroy" onClick={onDeleted} />
+        </div>
+        {editing ? <TaskEdit onEscPress={onEscPress} onBlur={onBlur} onEdit={onEdit} label={label} id={id} /> : null}
+      </li>
+    );
+  }
+}
 
 Task.defaultProps = {
   onDeleted: () => {},
@@ -45,6 +74,7 @@ Task.defaultProps = {
   onEdit: () => {},
   onBlur: () => {},
   onEscPress: () => {},
+  updateInterval: 5000,
 };
 
 Task.propTypes = {
@@ -58,7 +88,6 @@ Task.propTypes = {
   completed: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
+  updateInterval: PropTypes.number,
   time: PropTypes.instanceOf(Date).isRequired,
 };
-
-export default Task;
